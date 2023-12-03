02de4c9b319e7a4fae1f2852ce28f499603b20af
dom <50315102+DomGada@users.noreply.github.com>
2023-12-03 08:47:08 -0800

    Misread the problem fixed, didn't realize I was supposed to add the first and last numbers only but solved, also got the benefit of learning how to write to a file.

--- day1/day1.go
+++ day1/day1.go
@@ -5,16 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"unicode"
 )
 
 func final_write(strs []string) {
-	f, err := os.Create("answer.txt")
+	// f, err := os.Create("answer.txt")
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	bigprint := 0
 	for i := 0; i < len(strs); i++ {
 		newS := string(strs[i])
 		myprint := ""
@@ -22,25 +24,33 @@ func final_write(strs []string) {
 			if unicode.IsDigit([]rune(newS)[j]) {
 				myprint = myprint + string(newS[j])
 			}
-		}
 
-		l, err := f.WriteString(myprint)
-		if i < len(strs)-1 {
-			f.WriteString("\n")
 		}
+		addValue := string(myprint[0]) + string(myprint[len(myprint)-1])
+		add, err := strconv.Atoi(addValue)
 		if err != nil {
-			fmt.Println(err)
-			f.Close()
-			return
+			panic(err)
 		}
-		fmt.Println(l, "bytes written")
+		bigprint = bigprint + add
+
+		// l, err := f.WriteString(myprint)
+		// if i < len(strs)-1 {
+		// 	f.WriteString("\n")
+		// }
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	f.Close()
+		// 	return
+		// }
+		// fmt.Println(l, "bytes written")
 
 	}
-	err = f.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// err = f.Close()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	fmt.Println(bigprint)
 
 }
 
